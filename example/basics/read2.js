596cff39ed06288d2944899ad89fa29b4806eba2
Fred Yang <fredyang@semanticsworks.com>
2015-11-09 08:27:27 -0500

    add comment to read2.js which support stream.pipe method

--- example/basics/read2.js
+++ example/basics/read2.js
@@ -1,3 +1,6 @@
+//this program allow you pipe a readable stream to another
+//writable, the writable stream
+
 var Readable = require('stream').Readable;
 var rs = Readable();
 
@@ -5,7 +8,10 @@ var c = 97 - 1;
 
 rs._read = function () {
     if (c >= 'z'.charCodeAt(0)) return rs.push(null);
-    
+
+    //setTimeout is necessary because the operating system
+    //requires some time to send us the relevant signals to
+    //close the pipe
     setTimeout(function () {
         rs.push(String.fromCharCode(++c));
     }, 100);
@@ -13,7 +19,13 @@ rs._read = function () {
 
 rs.pipe(process.stdout);
 
+//this handler is necessary because the operating system will
+//send SIGPIPE to our process when writable stream is
+//no longer interested in readable output, which
+//gets emitted as EPIPE error on process.stdout
 process.on('exit', function () {
     console.error('\n_read() called ' + (c - 97) + ' times');
 });
+
+
 process.stdout.on('error', process.exit);
